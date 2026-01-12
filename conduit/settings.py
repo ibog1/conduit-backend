fc19cb6714a2bbea5a1f673973a4b365ab399b74
ibog1 <156073479+ibog1@users.noreply.github.com>
2026-01-12 23:19:26 +0100

    Update settings.py

--- conduit/settings.py
+++ conduit/settings.py
@@ -8,7 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = True
 
-ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '*').split(',')
+ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 INSTALLED_APPS = [
